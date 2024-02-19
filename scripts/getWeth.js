e80fa67fdef47b7536077bf03fa2083532d0f507
cinlends <2488732383@qq.com>
2024-02-19 20:02:49 +0800

    fix: user deposit signer，not deployer

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -3,10 +3,10 @@ const AMOUNT = ethers.parseEther("0.02");
 // 进行抵押，把 eth 换成 weth
 async function getWeth() {
 	const { deployer } = await getNamedAccounts();
+	const signer = await ethers.provider.getSigner();
 	// 需要调用 weth 合约的 deposit 方法
 	// 因此需要合约 abi, address
-
-	const iWeth = await ethers.getContractAt("IWeth", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", deployer);
+	const iWeth = await ethers.getContractAt("IWeth", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", signer);
 	const tx = await iWeth.deposit({ value: AMOUNT });
 	await tx.wait();
 	const wethBalance = await iWeth.balanceOf(deployer);
